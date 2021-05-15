86f431d38eafe98404136b7849fc7da5f4e20d4b
mandeepika <mandeepikasaini28@gmail.com>
2021-05-15 14:25:50 -0700

    display matrix (but using @validate + TraitError)

--- jupyterteam_widget/widget.py
+++ jupyterteam_widget/widget.py
@@ -3,6 +3,9 @@ from traitlets import Unicode, Int, validate, TraitError
 
 from ._frontend import module_name, module_version
 
+import sys
+import numpy as np
+
 # See js/lib/widget.js for the frontend counterpart to this file.
 
 @register
@@ -33,4 +36,44 @@ class HermiteWidget(DOMWidget, ValueWidget):
         proposal_value = proposal['value']
         if proposal_value < 0 or proposal_value > 10:
             raise TraitError('Invalid integer: accepted values are 0 <= value <= 10')
-        return proposal_value
+
+        # compute the matrix if the value entered is valid
+        compute_matrix(proposal_value)
+
+
+
+# we need to adapt this into the backend of the widget at jupyterteam_widget/widget.py
+# the idea is basically to replace sys.argv with the value of the widget
+#
+# Run this script with `python hermite.py n` where integer 5 < `n` < 264 is the dimension 
+# of the array for the coefficients of each hermite polynomial order k.
+# Bear in mind that the highest order hermite polynomial k and psi you will calculate is n-1.
+#
+# we use the physicists polynomials from here https://en.wikipedia.org/wiki/Hermite_polynomials#Definition
+
+def compute_matrix(self):
+    ORDER = int(self)
+
+    NARY = np.zeros((ORDER+1,ORDER+1))
+    NARY[0,0] = 1
+
+    if ORDER > 0:
+        NARY[1,1] = 2
+
+        # fill out the coefficient matrix using equation 4-16
+        # fails if n < 1
+        for k in range(ORDER+1):
+            for n in range(ORDER):
+                NARY[n+1,k] = 2*NARY[n,k-1] - 2*n*NARY[n-1,k]
+
+    print(NARY)
+
+# for a given rho, find the hermite polynomial of order n
+# def hermite(n,rho, NARY):
+#     coefficients = {}
+#     Hn = 0
+#     for k in range(n+1):
+#         coefficients[k] = NARY[n,k]
+#     for k in coefficients:
+#         Hn += coefficients[k]*rho**k
+#     return Hn
